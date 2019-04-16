08dffc60047b18487dcbce58efbfeac686ee7f90
Vishal-Chhetri <42584441+Vishal-Chhetri@users.noreply.github.com>
2019-04-17 01:23:43 +0530

    adding nav bar
    adding nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule ,routingComponent} from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { StudentdashboardComponent } from './studentdashboard/studentdashboard.component';
@@ -11,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminComponent } from './admin/admin.component';
 import { ParentComponent } from './parent/parent.component';
 import { TeacherComponent } from './teacher/teacher.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './design/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,15 +23,16 @@ import { TeacherComponent } from './teacher/teacher.component';
     AdminComponent,
     ParentComponent,
     TeacherComponent,
-    
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
-      
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
